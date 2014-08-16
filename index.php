1c8a17c741503b2e77ee9e515330016045e9cb07
Tan Bui <trungtanbui@gmail.com>
2014-08-16 22:35:33 +0700

    global object, lady dump

--- index.php
+++ index.php
@@ -1,34 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tanub
- * Date: 8/13/14
- * Time: 11:25 PM
- */
+require "vendor/autoload.php";
 
-class NameException extends Exception { }
-class EmailException extends Exception { }
+$a = new stdClass();
+$a->pro = 11;
+ladybug_dump($a);
 
-$name = "";
-$email= "";
+function hello(){
+    ladybug_dump($a);
+    $b = "temp b";
+}
 
-try {
-    if (empty($name)) {
-        throw new NameException();
-    }
-    elseif (empty($email)) {
-        throw new EmailException();
-    }
-    else {
-        echo "Name is " . $name . "<br>";
-        echo "Email is " . $email;
-    }
-}
-catch (NameException $n) {
-    echo "A name was not provided.";
-    error_log($n->getTraceAsString());
-}
-catch (EmailException $e) {
-    echo "An email address was not provided.";
-    error_log($e->getTraceAsString());
-}
+hello();
+
+ladybug_dump($a);
