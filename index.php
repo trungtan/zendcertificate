3c842a1d515b43b4a98b66a3b19f9a8bf5739c80
Tan Bui <trungtanbui@gmail.com>
2014-08-16 08:22:06 +0700

    set_error_handler

--- index.php
+++ index.php
@@ -6,4 +6,17 @@
  * Time: 11:25 PM
  */
 
-phpinfo();
+function handleError($errno, $errstr,$error_file,$error_line)
+{
+    echo "<b>Error:</b> [$errno] $errstr - $error_file:$error_line";
+    echo "<br />";
+    echo "Terminating PHP Script";
+    die();
+}
+
+set_error_handler("handleError");
+
+echo $asd;
+
+//$a = new stdClass();
+//echo $a->asd();
